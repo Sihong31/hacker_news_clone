4061c0aa66b2fa1afafcf39c5a8dbc21aa3c323d
Sihong31 <sihongspring2011@gmail.com>
2015-07-16 12:21:59 -0400

    added show view and method logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,12 +19,15 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find_by(id: params[:id])
   end
 
   def edit
+    @post = Post.find_by(id: params[:id])
   end
 
   def update
+
   end
 
   def destroy
